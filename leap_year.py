fa9b4a57fa0a5242619b5715c629b8b3c8fbe5dc
Ecassanm <78620296+Ecassanm@users.noreply.github.com>
2021-02-21 23:57:42 -0500

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,12 +1,15 @@
 year = input("Please input a year: ")
 if (year % 100) == 0:
 	if (year % 400) == 0:
-		print("In the year %4d, there are 29 days in February." % (year))
-	else:
-		print("In the year %4d, there are 28 days in February." % (year))
+		days = 29
+		print("In the year 2000, there are 29 days in February" % (year, days))
+   else:
+		days = 28
+		print("In the year %4d, there are %2d days in February." % (year, days))
 else:
 	if (year % 4) == 0:
-		print("In the year %4d, there are 29 days in February." % (year))
-
+		days = 29
+		print("In the year %4d, there are %2d days in February." % (year, days))
 	else:
-		print("In the year %4d, there are 28 days in February." % (year))
+		days = 28
+		print("In the year %4d, there are %2d days in February." % (year, days))
