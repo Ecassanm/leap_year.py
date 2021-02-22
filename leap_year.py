18abaf812f9301ee3c3efb00f4200277247cb26a
Ecassanm <78620296+Ecassanm@users.noreply.github.com>
2021-02-22 00:40:18 -0500

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -6,7 +6,7 @@
 ################################################################################
 
 year = input("Please input a year: ")
-if (year % 100) > 0:
+if (year % 100) == 0:
 	if (year % 400) == 0:
 		days = 29
 		print("In the year, ", year, " there are " , days, " days in February.")
@@ -14,7 +14,7 @@ if (year % 100) > 0:
 		days = 28
 		print("In the year, ", year, " there are " , days, " days in February.")
 else:
-	if (year % 4) > 0:
+	if (year % 4) == 0:
 		days = 29
 		print("In the year, ", year, " there are " , days, " days in February.")
 	else: 
